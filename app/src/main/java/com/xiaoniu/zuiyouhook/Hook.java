e0babe042413f1620ed356eba5c486eeaa3aafb2
KuaiLeXiaoNiu <1422896109@qq.com>
2023-01-28 19:22:14 +0800

    change hook points

--- app/src/main/java/com/xiaoniu/zuiyouhook/Hook.java
+++ app/src/main/java/com/xiaoniu/zuiyouhook/Hook.java
@@ -14,42 +14,19 @@ public class Hook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals("cn.xiaochuankeji.tieba")) {
-            //XposedBridge.log("Loaded app: " + lpparam.packageName);
-            //腾讯
-            XposedHelpers.findAndHookMethod("com.qq.e.comm.managers.b", lpparam.classLoader, "d", hookFalse);
-            //穿山甲
-            XposedHelpers.findAndHookMethod("com.bytedance.sdk.openadsdk.TTAdConfig", lpparam.classLoader, "getSdkInfo", hookNull);
-            //趣盟
-            XposedHelpers.findAndHookMethod("com.qumeng.advlib.api.AiClkAdManager", lpparam.classLoader, "init", Context.class, String.class, replaceNull);
-            //疑似自建SDK
-            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.xcad.sdk.XcADSdk", lpparam.classLoader, "init", Application.class, String.class, replaceNull);
-            //小米 mimo
-            XposedHelpers.findAndHookMethod("com.miui.zeus.mimo.sdk.MimoSdk", lpparam.classLoader, "init",
-                    Context.class, XposedHelpers.findClass("com.miui.zeus.mimo.sdk.MimoSdk$InitCallback", lpparam.classLoader), replaceNull);
-            //游可盈
-            XposedHelpers.findAndHookMethod("com.tencent.klevin.KlevinManager", lpparam.classLoader, "init",
-                    Context.class, XposedHelpers.findClass("com.tencent.klevin.KlevinConfig", lpparam.classLoader),
-                    XposedHelpers.findClass("com.tencent.klevin.listener.InitializationListener", lpparam.classLoader), replaceNull);
-            //快手
-            XposedHelpers.findAndHookMethod("com.kwad.sdk.core.network.BaseResultData", lpparam.classLoader, "isResultOk", hookFalse);
-            XposedHelpers.findAndHookMethod("com.kwad.components.offline.api.core.network.model.BaseOfflineCompoResultData", lpparam.classLoader, "isResultOk", hookFalse);
+            Class a = XposedHelpers.findClass("cn.xiaochuankeji.hermes.core.provider.ADSDKInitParam", lpparam.classLoader), b = XposedHelpers.findClass("kotlin.coroutines.Continuation", lpparam.classLoader);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.bjxingu.BJXinguADProvider", lpparam.classLoader, "init", a, b, replaceNull);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.klevin.KlevinADProvider", lpparam.classLoader, "init", a, b, replaceNull);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.kuaishou.KuaishouADProvider", lpparam.classLoader, "init", a, b, replaceNull);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.mimo.MimoADProvider", lpparam.classLoader, "init", a, b, replaceNull);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.pangle.PangleADProvider", lpparam.classLoader, "init", a, b, replaceNull);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.qumeng.QuMengADProvider", lpparam.classLoader, "init", a, b, replaceNull);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.tencent.TencentADProvider", lpparam.classLoader, "init", a, b, replaceNull);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.xcad.XcADProvider", lpparam.classLoader, "init", a, b, replaceNull);
+            XposedHelpers.findAndHookMethod("cn.xiaochuankeji.hermes.xingu.XinguADProvider", lpparam.classLoader, "init", a, b, replaceNull);
         }
     }
 
-    XC_MethodHook hookFalse = new XC_MethodHook() {
-        @Override
-        //在被hook的方法执行之后执行这里的代码
-        protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-            param.setResult(false);
-        }
-    };
-    XC_MethodHook hookNull = new XC_MethodHook() {
-        @Override
-        //在被hook的方法执行之后执行这里的代码
-        protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-            param.setResult(null);
-        }
-    };
     XC_MethodReplacement replaceNull = new XC_MethodReplacement() {
         @Override
         //拦截执行
